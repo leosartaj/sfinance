3823d15ae732942464341aec9e25caea3864adb0
Sartaj Singh <singhsartaj94@gmail.com>
2014-02-18 14:37:31 -0800

    rm checker.php

--- checker.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once('includes/PasswordHash.php');
-	$hasher = new PasswordHash(8, false);
-	$hash = $hasher->HashPassword('database');
-	$check = $hasher->CheckPassword('database', $hash);
-	print($check);
-?>
