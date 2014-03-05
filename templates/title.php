0184025c019e66c719e20830c54dabdd20cfa9d8
Sartaj Singh <singhsartaj94@gmail.com>
2014-03-04 17:17:44 -0800

    bug fix-comment

--- templates/title.php
+++ templates/title.php
@@ -2,12 +2,10 @@
 <html>
 <head>
 	<title>
-	<!-- Dynamic title using helper.php, render function -->
 		<?= $title ?>
 	</title>
 	<meta name="viewport"  content="width-device-width, initial-scale=1.0" >
 	<base href="http://localhost/sfinance/controller/">
-	<!-- Required scripts and links -->
 	<link rel="stylesheet" type="text/css" href="../views/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="../views/css/custom.css">
+	<link rel="stylesheet" type="text/css" href="../views/css/footer.css">
 	<script src="../views/js/jquery-2.1.0.min.js"></script>
